e93fd4be35714e353816c58244266a7e2ea8a4a3
Се Вон Хан <skhan_1@edu.hse.ru>
2022-04-21 01:43:46 +0600

    add import

--- lib/src/widgets/payment_widget.dart
+++ lib/src/widgets/payment_widget.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_paybox/src/flutter_paybox.dart';
 import 'package:webview_flutter/webview_flutter.dart';
